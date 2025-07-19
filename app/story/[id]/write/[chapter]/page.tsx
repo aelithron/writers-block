bc746b8a14d2970ec9527798c3f4600de78d0de1
Aelithron <aelithron@gmail.com>
2025-07-19 01:17:38 -0600

    Start writing the chapter edit page, start adding auth Going to go work on BookLink because auth is the bane of my existence

---
+++ app/story/[id]/write/[chapter]/page.tsx
@@ -0,0 +1,7 @@
+export default async function Page({ params }: { params: Promise<{ id: string, chapter: string }> }) {
+  return (
+    <main className="flex flex-col p-8 md:p-20 min-h-screen gap-4">
+      
+    </main>
+  )
+}
